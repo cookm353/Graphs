38e8d43b844342c279db9d5161a45ab5f056e927
Matthew Cook <cookm353@gmail.com>
2023-11-22 14:29:26 -0500

    updating progress

--- graph.js
+++ graph.js
@@ -11,25 +11,63 @@ class Graph {
   }
 
   // this function accepts a Node instance and adds it to the nodes property on the graph
-  addVertex(vertex) {}
+  addVertex(vertex) {
+    this.nodes.add(vertex)
+  }
 
   // this function accepts an array of Node instances and adds them to the nodes property on the graph
-  addVertices(vertexArray) {}
+  addVertices(vertexArray) {
+    for (let v of vertexArray) {
+      this.nodes.add(v)
+    }
+  }
 
   // this function accepts two vertices and updates their adjacent values to include the other vertex
-  addEdge(v1, v2) {}
+  addEdge(v1, v2) {
+    v1.adjacent.add(v2)
+    v2.adjacent.add(v1)
+  }
 
   // this function accepts two vertices and updates their adjacent values to remove the other vertex
-  removeEdge(v1, v2) {}
+  removeEdge(v1, v2) {
+    v1.adjacent.delete(v2)
+    v2.adjacent.delete(v1)
+  }
 
   // this function accepts a vertex and removes it from the nodes property, it also updates any adjacency lists that include that vertex
-  removeVertex(vertex) {}
+  removeVertex(vertex) {
+    this.nodes.delete(vertex)
+  }
 
   // this function returns an array of Node values using DFS
-  depthFirstSearch(start) {}
+  depthFirstSearch(start) {
+    let toVisitStack = [start]
+    const seen = new Set(toVisitStack)
+  }
 
   // this function returns an array of Node values using BFS
-  breadthFirstSearch(start) {}
+  breadthFirstSearch(start) {
+    let toVisitQueue = [start]
+    const seen = new Set(toVisitQueue)
+    const visitedNodes = [start.value]
+
+    while (toVisitQueue.length) {
+      let currPerson = toVisitQueue.shift()
+
+      for (let neighbor of currPerson.adjacent) {
+        if (!seen.has(neighbor)) {
+          seen.add(neighbor)
+          toVisitQueue.push(neighbor)
+          visitedNodes.push(neighbor.value)
+        }
+      }
+    }
+    return visitedNodes
+  }
 }
 
 module.exports = {Graph, Node}
+
+const foo = new Set()
+
+foo.dele
--- scratch.js
+++ scratch.js
@@ -1,76 +1,154 @@
-// Adjacency matrix
-const graph = [
-    [0, 1, 0],
-    [1, 0, 1],
-    [0, 1, 0]
+class Node {
+    constructor(name) {
+        this.name = name
+        this.adjacent = []
+    }
+    addEdge(...names) {
+        for (let name of names) {
+            this.adjacent.push(name)
+        }
+    }
+}
+
+const seal = new Node("Seal")
+const penguin = new Node("Penguin")
+const fish = new Node("Fish")
+const squid = new Node("Squid")
+
+seal.addEdge(penguin, fish, squid)
+// console.log(seal)
+
+const adjacencyList = {
+    A: ["B", "F"],
+    B: ["A", "C"],
+    C: ["B", "D"],
+    D: ["C", "E"],
+    E: ["D", "F"],
+    F: ["E", "A"]
+}
+
+// console.log(adjacencyList.A)
+
+const adjacencyMatrix = [
+    [0, 1, 0, 0, 0, 1],
+    [1, 0, 1, 0, 0, 0],
+    [0, 1, 0, 1, 0, 0],
+    [0, 0, 1, 0, 1, 0],
+    [0, 0, 0, 1, 0, 1],
+    [1, 0, 0, 0, 1, 0]
 ]
 
-console.log(graph[0][1])
-
-class SimpleMap {
-    constructor() {
-        this._items = []
+class PersonNode {
+    constructor(name, adjacent = new Set()) {
+        this.name = name
+        this.adjacent = adjacent
     }
-
-    set(k, v) {
-        this._items.push([k, v])
+    showConnections() {
+        for (let person of this.adjacent) {
+            console.log(person.name)
+        }
     }
-
-    get(k) {
-        let kv = this._items.find(kv => k === kv[0])
-        return kv ? kv[1] : undefined
-    }
-
-    has(k) {
-        return this._items.find(kv => k === kv[0]) !== undefined
-    }
-
-    delete(k) {
-        let i = this._items.findItem(kv => k === kv[0])
-        if (i !== -1) this._items.splice(i, 1)
-    }
-
-    keys() { return this._items.map(kv => kv[0])}
-    values() { return this._items.map(kv => kv[1])}
-    entries() { return this._items}
 }
 
-const newMap = new SimpleMap()
-
-newMap.set("foo", "foo")
-newMap.set("bar", "bar")
-newMap.set("baz", "baz")
-
-console.log(newMap.get("bar"))
-console.log(newMap.has("baz"))
-
-
-class HashMap {
+class FriendGraph {
     constructor() {
-        this._items = []
+        this.nodes = new Set()
     }
-    hash(key, arrayLength) {
-        const H_PRIME = 31
-        let hash = Array.from(key).reduce(
-            (accum, char) => accum * H_PRIME + char.charCodeAt(), 0)
+    /**
+     * Add a person to list
+     * @param {PersonNode} person Person to be added to the graph
+     */
+    addPerson(person) {
+        this.nodes.add(person)
+    }
+    /**
+     * 
+     * @param  {...PersonNode} people Comma separated list of people to be added to graph
+     */
+    addPeople(...people) {
+        for (let p of people) {
+            this.addPerson(p)
+        }
+    }
+    /**
+     * Adds connection between the two people
+     * @param {PersonNode} person1 First person to be 
+     * @param {PersonNode} person2 
+     */
+    setFriends(person1, person2) {
+        person1.adjacent.add(person2)
+        person2.adjacent.add(person1)
+    }
+    /**
+     * Checks if two people are connected
+     * @param {PersonNode} person1 
+     * @param {PersonNode} person2 
+     * 
+     * @returns {boolean}
+     */
+    areConnectedBFS(person1, person2) {
+        let toVisitQueue = [person1]
+        const seen = new Set(toVisitQueue)
+        while (toVisitQueue.length) {
+            let currPerson = toVisitQueue.shift()
 
+            if (currPerson == person2) return true
+
+            for (let neighbor of currPerson.adjacent) {
+                if (!seen.has(neighbor)) {
+                    toVisitQueue.push(neighbor)
+                    seen.add(neighbor)
+                }
+            }
+        }
+
+        return false
+    }
+    areConnectedIterativeDFS(person1, person2) {
+        const toVisitStack = [person1]
+        const seen = new Set(toVisitStack)
+
+        while(toVisitStack.length) {
+            const currPerson = toVisitStack.pop()
+
+            if (currPerson == person2) return true
+
+            for (let neighbor of currPerson.adjacent) {
+                if(!seen.has(neighbor)) {
+                    toVisitStack.push(neighbor)
+                    seen.add(neighbor)
+                }
+            }
+        }
         
-        return hash % arrayLength
+        return false
     }
-    set(k, v) {
-        const hashedKey = this.hash(k)
-        this._items[hashedKey] = v
-    }
-    get(k) {
-        const hashedKey = this.hash(k)
-        return this._items[hashedKey]
+    areConnectedRecursiveDFS(person1, person2) {
+
     }
 }
 
-const fruit = new HashMap()
-fruit.set("apple", "red")
-fruit.set("grape", "purple")
-fruit.set("lime", "green")
+const homer = new PersonNode("Homer")
+const marge = new PersonNode("Marge")
+const maggie = new PersonNode("Maggie")
+const lisa = new PersonNode("Lisa")
+const grampa = new PersonNode("Grampa")
+const moe = new PersonNode("Moe")
+const barney = new PersonNode("Barney")
+const lenny = new PersonNode("Lenny")
+const friendGraph = new FriendGraph()
 
-console.log(fruit.get("orange"))
-console.log(fruit._items)
+friendGraph.setFriends(homer, marge)
+friendGraph.setFriends(homer, maggie)
+friendGraph.setFriends(homer, lisa)
+friendGraph.setFriends(marge, maggie)
+friendGraph.setFriends(maggie, lisa)
+friendGraph.setFriends(lisa, grampa)
+friendGraph.setFriends(moe, barney)
+friendGraph.setFriends(barney, lenny)
+friendGraph.addPeople(homer, marge, lisa, maggie, grampa, moe, barney, lenny)
+
+console.log(friendGraph.areConnectedBFS(marge, grampa))
+console.log(friendGraph.areConnectedBFS(marge, moe))
+console.log(friendGraph.areConnectedIterativeDFS(marge,grampa))
+console.log(friendGraph.areConnectedIterativeDFS(marge, moe))
